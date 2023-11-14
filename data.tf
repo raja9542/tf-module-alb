592061fa8601f804d3c1d43cd0415fe0731d05a2
raja9542 <rajasekhar1banda@gmail.com>
2023-11-14 21:01:45 +0530

    rabbitmq service creation

---
+++ data.tf
@@ -0,0 +1,3 @@
+# we need public load balancer created in public subnet and 1 private load balancer to be created in app subnet.
+# public load balancer is exposed to outside world
+# private load balancer is for interaction b/w web to app subnets
---
+++ main.tf
@@ -0,0 +1,25 @@
+resource "aws_security_group" "rabbitmq" {
+  name        = "${var.env}-rabbitmq-security-group"
+  description = "${var.env}-rabbitmq-security-group" // any name
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description      = "rabbitmq"
+    from_port        = 5672
+    to_port          = 5672
+    protocol         = "tcp"
+    cidr_blocks      = var.allow_cidr // to allow app cidr block
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = merge(
+    local.common_tags,
+    {Name = "${var.env}-rabbitmq-security-group"}
+  )
+}
