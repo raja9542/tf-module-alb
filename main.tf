b7253a6440aa469bd5ecbc9d0ed1412b6213c299
raja9542 <rajasekhar1banda@gmail.com>
2023-11-14 21:35:45 +0530

    rabbitmq service creation

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
-resource "aws_security_group" "rabbitmq" {
-  name        = "${var.env}-rabbitmq-security-group"
-  description = "${var.env}-rabbitmq-security-group" // any name
+resource "aws_security_group" "main" {
+  name        = "${var.env}-alb-${var.subnets_name}-security-group"
+  description = "${var.env}-alb-${var.subnets_name}-security-group" // any name
   vpc_id      = var.vpc_id
 
   ingress {
-    description      = "rabbitmq"
-    from_port        = 5672
-    to_port          = 5672
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
     protocol         = "tcp"
     cidr_blocks      = var.allow_cidr // to allow app cidr block
   }
@@ -20,6 +20,6 @@ resource "aws_security_group" "rabbitmq" {
 
   tags = merge(
     local.common_tags,
-    {Name = "${var.env}-rabbitmq-security-group"}
+    {Name = "${var.env}-alb-security-group"}
   )
 }
